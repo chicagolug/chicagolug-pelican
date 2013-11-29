c1a5a45ca125c618ac002d2102acdde9981b1d77
Konstantin Ryabitsev <mricon@kernel.org>
2013-11-29 14:34:51 -0500

    Mark 3.11.x as EOL per Greg
    http://lkml.kernel.org/g/20131129190948.GA30832@kroah.com

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ sys.path.append('./')
 from plugins import releases
 
 LONGTERM_KERNELS = ('3.10', '3.4', '3.2', '3.0', '2.6.34', '2.6.32')
-EOL_KERNELS = ('3.0', '3.5', '3.6', '3.7', '3.8', '3.9')
+EOL_KERNELS = ('3.0', '3.5', '3.6', '3.7', '3.8', '3.9', '3.11')
 
 GIT_MAINLINE = '/mnt/git-repos/repos/pub/scm/linux/kernel/git/torvalds/linux.git'
 GIT_STABLE   = '/mnt/git-repos/repos/pub/scm/linux/kernel/git/stable/linux-stable.git'
